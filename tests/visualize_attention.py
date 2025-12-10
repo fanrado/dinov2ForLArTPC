234db3da82ffe0e4eb0dbe88beebb281af677478
fanrado <radofana@gmail.com>
2025-12-10 11:25:58 -0800

    If threshold is provided, show the attention map as a mask overlapping the original image

--- tests/visualize_attention.py
+++ tests/visualize_attention.py
@@ -37,6 +37,70 @@ from transformers import AutoImageProcessor, AutoModel, AutoConfig
 sys.path.append('../')
 from dinov2.models.vision_transformer import vit_small, vit_large
 
+def apply_mask(image, mask, color, alpha=0.5):
+    print('Applying mask to the image')
+    for c in range(3):
+        print(f'Image shape : {image.shape}')
+        print(f'Mask shape : {mask.shape}')
+        print(f'Color : {color[c]}')
+        print(f'alpha : {alpha}')
+        image[:, :, c] = image[:, :, c] * (1 - alpha * mask) + alpha * mask * color[c] * 255
+    return image
+
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
+
+
+def display_instances(image, mask, fname="test", figsize=(5, 5), blur=False, contour=True, alpha=0.5):
+    fig = plt.figure(figsize=figsize, frameon=False)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax = plt.gca()
+
+    N = 1
+    mask = mask[None, :, :]
+    # Generate random colors
+    colors = random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    margin = 0
+    ax.set_ylim(height + margin, -margin)
+    ax.set_xlim(-margin, width + margin)
+    ax.axis('off')
+    masked_image = np.asarray(image).astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+        _mask = mask[i]
+        if blur:
+            _mask = cv2.blur(_mask,(10,10))
+        # Mask
+        masked_image = apply_mask(masked_image, _mask, color, alpha)
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        if contour:
+            padded_mask = np.zeros((_mask.shape[0] + 2, _mask.shape[1] + 2))
+            padded_mask[1:-1, 1:-1] = _mask
+            contours = find_contours(padded_mask, 0.5)
+            for verts in contours:
+                # Subtract the padding and flip (y, x) to (x, y)
+                verts = np.fliplr(verts) - 1
+                p = Polygon(verts, facecolor="none", edgecolor=color)
+                ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8), aspect='auto')
+    fig.savefig(fname)
+    print(f"{fname} saved.")
+    return
+
 if __name__ == '__main__':
     # image_size = (952, 952)
     image_size = (480, 480)
@@ -106,11 +170,27 @@ if __name__ == '__main__':
     print(f'attention shape : {attentions.shape}, {nh}')
     
     attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
+
+    ## apply mask
+    threshold = 0.6
+    if threshold is not None:
+        # we keep only a certain percentage of the mass
+        val, idx = torch.sort(attentions)
+        val /= torch.sum(val, dim=1, keepdim=True)
+        cumval = torch.cumsum(val, dim=1)
+        th_attn = cumval > (1 - threshold)
+        idx2 = torch.argsort(idx)
+        for head in range(nh):
+            th_attn[head] = th_attn[head][idx2[head]]
+        th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+        # interpolate
+        th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=patch_size, mode="nearest")[0].cpu().numpy()
+
     # attentions = attentions[0, :, 0:].reshape(nh, -1)
     print(attentions.shape)
     # weird: one pixel gets high attention over all heads?
     print(torch.max(attentions, dim=1)) 
-    # attentions[:, 283] = 0 
+    attentions[:, 283] = 0 
 
     ##
     attentions = attentions.reshape(nh, w_featmap, h_featmap)
@@ -125,4 +205,17 @@ if __name__ == '__main__':
         plt.imsave(fname=fname, arr=attentions[j], format='jpg')
         print(f"{fname} saved.")
 
+    if threshold is not None:
+        image = skimage.io.imread(os.path.join('.', "cow-beach.jpg"))
+        print(f'Image shape : {image.shape}')
+        image = np.asarray(image)
+        print(f'Image shape after skimage.io.imread : {image.shape}')
+
+        # make the image divisible by the patch size
+        w, h = image.shape[0] - image.shape[0] % patch_size, image.shape[1] - image.shape[1] % patch_size
+        print(f'cropped size: {(w, h)}')
+
+        image = image[:w, :h, :]
+        for j in range(nh):
+            display_instances(image, th_attn[j], fname=os.path.join(output_dir, "mask_th" + str(threshold) + "_head" + str(j) +".jpg"), blur=False)
 

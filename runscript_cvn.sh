941854f633921bdecd573e8d45fa1a122b1672a9
fanrado <radofana@gmail.com>
2026-01-09 07:48:28 -0800

    latest change to train the model and get the output checkpoints in out_cvn_memlite_trainworkers2_batchsize8/

--- runscript_cvn.sh
+++ runscript_cvn.sh
@@ -13,10 +13,9 @@ torchrun --standalone --nproc_per_node=1 \
   --output-dir out_cvn_memlite \
   train.dataset_path="CVN:root=/nfs/data/1/rrazakami/work/data_cvn/data/dune/2023_trainings/latest/dunevd:extra=plane=Z,mono=mono3"\
   train.num_workers=2 \
-  train.global_crops_size=168 train.local_crops_size=64 train.local_crops_number=2 \
-  train.batch_size_per_gpu=2 train.accum_iter=4 \
+  crops.global_crops_size=168 crops.local_crops_size=56 crops.local_crops_number=8 \
+  train.batch_size_per_gpu=8 train.accum_iter=4 \
   model.mixed_precision.param_dtype=bf16 model.mixed_precision.reduce_dtype=bf16 model.mixed_precision.buffer_dtype=bf16 \
   train.autocast_dtype=bf16 \
-  fsdp.use_fsdp=false model.use_fsdp=false train.use_fsdp=false
-
+  fsdp.use_fsdp=false model.use_fsdp=false train.use_fsdp=false \
 
